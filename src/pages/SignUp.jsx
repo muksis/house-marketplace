e6f266daf0219c076908ad6adbd457b559ee73a5
muksis <60219890+muksis@users.noreply.github.com>
2022-04-28 17:29:30 -0400

    Add register user

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { db } from "../firebase.config";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 
@@ -21,6 +23,26 @@ function SignUp() {
       }));
     };
 
+    const onSubmit = async (e) => {
+      e.preventDefault();
+
+      try {
+        const auth = getAuth();
+
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+
+        const user = userCredential.user;
+
+        updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+
+        navigate("/");
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
     return (
       <>
         <div className="pageContainer">
@@ -29,7 +51,7 @@ function SignUp() {
           </header>
 
           <main>
-            <form>
+            <form onSubmit={onSubmit}>
               <input type="text" className="nameInput" placeholder="name" id="name" value={name} onChange={onChange} />
 
               <input type="email" className="emailInput" placeholder="Email" id="email" value={email} onChange={onChange} />
