4fb7900a6a712409f647f05331a79b3def7460a2
muksis <60219890+muksis@users.noreply.github.com>
2022-05-07 14:10:28 -0400

    Add Google OAuth

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -4,6 +4,7 @@ import { toast } from "react-toastify";
 import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { setDoc, doc, serverTime, serverTimestamp } from "firebase/firestore";
 import { db } from "../firebase.config";
+import OAuth from "../components/OAuth";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 
@@ -82,7 +83,7 @@ function SignUp() {
               </div>
             </form>
 
-            {/* Google OAuth */}
+            <OAuth />
 
             <Link to="/sign-in" className="registerLink">
               Sign In Instead
