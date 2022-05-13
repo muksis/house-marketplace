7ac26c1a76c73a63b7ce0865ab967371bf3cf684
muksis <60219890+muksis@users.noreply.github.com>
2022-05-13 19:16:39 -0400

    Add listing details

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -17,7 +17,7 @@ function Listing() {
 
   useEffect(() => {
     const fetchListing = async () => {
-      const docRef = doc(db, "Listings", params.listingId);
+      const docRef = doc(db, "listings", params.listingId);
       const docSnap = await getDoc(docRef);
 
       if (docSnap.exists()) {
@@ -29,10 +29,70 @@ function Listing() {
     fetchListing();
   }, [navigate, params.listingId]);
 
+  if (loading) {
+      return <Spinner />
+  }
+
   return (
-    <div>
-      LISTING
-    </div>
+    <main>
+      {/* SLIDER */}
+
+      <div className="shareIconDiv" onClick={() => {
+        navigator.clipboard.writeText(window.location.href);
+        setShareLinkCopied(true);
+        setTimeout(() => {
+          setShareLinkCopied(false);
+        }, 2000);
+      }}>
+          <img src={shareIcon} alt="share" />
+      </div>
+
+      {shareLinkCopied && <p className="linkCopied">Link Copied!</p>}
+      
+      <div className="listingDetails">
+        <p className="listingName">
+          {console.log(listing)}
+          {listing.name} - $
+          {listing.offer
+            ? listing.discountedPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+            : listing.regularPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+        </p>
+        <p className="listingLocation">{listing.location}</p>
+        <p className="listingType">
+          For {listing.type === "rent" ? "Rent" : "Sale"}
+        </p>
+        {listing.offer && (
+          <p className="discountPrice">
+            ${listing.regularPrice - listing.discountedPrice} discount
+          </p>
+        )}
+
+        <ul className="listingDetailsList">
+          <li>
+            {listing.bedrooms > 1 ? `${listing.bedrooms} Bedrooms` : "1 bedroom"}
+          </li>
+          <li>
+            {listing.bathrooms > 1 ? `${listing.bathrooms} Bathrooms` : "1 bathroom"}
+          </li>
+          <li>{listing.parking && "Parking Spot"}</li>
+          <li>{listing.furnished && "Furnished"}</li>
+        </ul>
+
+        <p className="listingLocationTitle">Location</p>
+
+        {/* MAP */}
+
+        {auth.currentUser?.uid !== listing.userRef && (
+          <Link to={`/contacts/${listing.userRef}?listingName=${listing.name}&listingLocation=${listing.location}`} className="primaryButton">
+            Contact Landlord
+          </Link>
+        )}
+      </div>
+    </main>
   )
 }
 
